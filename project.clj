748eb7ba79b4b16f284e1375c36c483a18e71b4d
Sara Meisburger <sarameisburger@gmail.com>
2017-12-06 09:58:51 -0800

    (maint) Fix artifactory url Both resolve to the same place, this one is better

--- project.clj
+++ project.clj
@@ -24,8 +24,8 @@
                    :inherit [:managed-dependencies]}
   :plugins [[lein-parent "0.3.1"]]
 
-  :repositories [["releases" "https://artifactory.delivery.puppetlabs.net/artifactory/list/clojure-releases__local/"]
-                 ["snapshots"  "https://artifactory.delivery.puppetlabs.net/artifactory/list/clojure-snapshots__local/"]]
+  :repositories [["releases" "https://artifactory.delivery.puppetlabs.net/artifactory/clojure-releases__local/"]
+                 ["snapshots"  "https://artifactory.delivery.puppetlabs.net/artifactory/clojure-snapshots__local/"]]
 
-  :deploy-repositories [["releases" ~(deploy-info "https://artifactory.delivery.puppetlabs.net/artifactory/list/clojure-releases__local/")]
-                        ["snapshots" ~(deploy-info "https://artifactory.delivery.puppetlabs.net/artifactory/list/clojure-snapshots__local/")]])
+  :deploy-repositories [["releases" ~(deploy-info "https://artifactory.delivery.puppetlabs.net/artifactory/clojure-releases__local/")]
+                        ["snapshots" ~(deploy-info "https://artifactory.delivery.puppetlabs.net/artifactory/clojure-snapshots__local/")]])
