7006e5f5cc1889241e6b26d5104452edb08d9b39
Jenkins CI <ci@puppet.com>
2019-04-12 22:24:58 +0000

    Version 1.0.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
    :password :env/nexus_jenkins_password
    :sign-releases false})
 
-(defproject puppetlabs/analytics-client "1.0.0"
+(defproject puppetlabs/analytics-client "1.0.1-SNAPSHOT"
   :description "A library for submitting metrics to the trapperkeeper analytics service."
   :url "https://github.com/puppetlabs/analytics-client"
   :license {:name "Eclipse Public License"
